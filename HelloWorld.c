4eb4d037289b7ba66e7f90ba396d4d00430d8cc1
MM <github@mobildata.de>
2020-07-27 23:27:11 +0200

    HelloWorld.c as the source code of a project

---
+++ HelloWorld.c
@@ -0,0 +1,10 @@
+/* some program in C */
+
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello World!\n");
+	printf("(this message was coded in C language\n");
+	printf(" and compiled by BinaryBuilder.jl)\n");
+	return 0;
+}
